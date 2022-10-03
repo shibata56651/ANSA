c7f5443f55974e0267d5575522b29fda54c97627
shibata-taiga <shibata-taiga@mitsue.co.jp>
2022-10-03 11:01:37 +0900

    変更

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,40 @@
-const { src, dest, series, gulp, watch } = require("gulp");
-const ejs = require("gulp-ejs");
-const rename = require("gulp-rename");
-const imagemin = require('gulp-imagemin');
+const { src, dest, series, gulp, watch } = require('gulp');
+const ejs = require('gulp-ejs');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync');
 
-function ejsCompile(done) {
-  src(["_dev/ejs/**/*.ejs", "!" + "_dev/ejs/inc/*.ejs"])
+const ejsCompile = (done) => {
+  src(['_dev/ejs/**/*.ejs', '!' + '_dev/ejs/inc/*.ejs'])
     .pipe(ejs())
-    .pipe(rename({ extname: ".html" }))
-    .pipe(dest("docs/"));
+    .pipe(rename({ extname: '.html' }))
+    .pipe(dest('docs/'));
   done();
-}
+};
 
-function imageMin() {
-  gulp.src('./_dev-renew/imagemin/**/*')
-  .pipe(imagemin([
-    imagemin.gifsicle({ interlaced: true }),
-    imagemin.mozjpeg({ quality: 75, progressive: true }),
-    imagemin.optipng({ optimizationLevel: 5 }),
-    // imagemin.svgo({
-    //     plugins: [
-    //         {removeViewBox: true},
-    //         {cleanupIDs: false}
-    //     ]
-    // })
-  ]))
-  .pipe(gulp.dest('./positive/'))
-}
+/**
+ * ローカルサーバー立ち上げ
+ */
+const browserSyncFunc = () => {
+  browserSync.init(browserSyncOption);
+};
+
+const browserSyncOption = {
+  server: './docs/',
+};
+
+/**
+ * リロード
+ */
+const browserSyncReload = (done) => {
+  browserSync.reload();
+  done();
+};
 
 // ファイルの監視
 function watchTask(done) {
-  watch(["_dev/ejs/**/*.ejs"], ejsCompile);
+  watch(['_dev/ejs/**/*.ejs'], ejsCompile);
+  watch(['_dev/ejs/**/*.ejs', '_dev/scss/**/*.scss', '_dev/js/**/*.js'], browserSyncReload);
   done();
 }
 
-exports.default = series(ejsCompile, watchTask, imageMin);
+exports.default = series(ejsCompile, watchTask, browserSyncFunc);
